e2bfd2534fce1b3a2c76f347390396047ce7c6d0
PavelLebed20 <pavellebed30@gmail.com>
2019-10-28 23:44:28 +0300

    singleton_programm_runner

--- lebed.pavel/lab1/main.cpp
+++ lebed.pavel/lab1/main.cpp
@@ -2,71 +2,7 @@
 #include "logger.hpp"
 #include "pid.hpp"
 #include "files_mover.hpp"
-
-static ConfigReader g_conf_reader;
-static Pid g_pid(g_pid_file);
-static FilesMover g_file_mover;
-static bool g_work = true;
-
-static void start_proc()
-{
-	while (true)
-	{
-		while (!g_work)
-			sleep(1);
-		if (!g_file_mover.MoveFiles())
-			exit(EXIT_FAILURE);
-	}
-}
-
-static void parse_config()
-{
-	int update_interval = 0, old_interval = 0;
-	string src, dst;
-
-	if (!g_conf_reader.GetAsStr(g_folder1_key, src) ||
-		!g_conf_reader.GetAsStr(g_folder2_key, dst) ||
-		!g_conf_reader.GetAsInt(g_update_interval_key, update_interval) ||
-		!g_conf_reader.GetAsInt(g_old_interval_key, old_interval))
-		exit(EXIT_FAILURE);
-
-	if (update_interval<= 0 || old_interval <= 0)
-	{
-		if (update_interval <= 0)
-			g_logger.LogError("Update interval is not positive!");
-
-		if (old_interval <= 0)
-			g_logger.LogError("Check file lifetime interval is not positive!");
-		exit(EXIT_FAILURE);
-	}
-
-	g_file_mover = FilesMover(src, dst, (time_t)update_interval, (time_t)old_interval);
-}
-
-static void signal_handler(int sig)
-{
-	switch (sig)
-	{
-	case SIGTERM:
-		// Over process
-		g_logger.LogNotice("Teminate signal");
-		g_pid.Destroy();
-		exit(EXIT_SUCCESS);
-		break;
-	case SIGHUP:
-		g_work = false;
-		
-		g_conf_reader.Parse();
-		parse_config();
-		g_logger.LogNotice("Hangup signal");
-
-		g_work = true;
-		break;
-	default:
-		g_logger.LogNotice("Unknown signal");
-		break;
-	}
-}
+#include "runner.hpp"
 
 int main(int argc, char** argv)
 {
@@ -93,14 +29,14 @@ int main(int argc, char** argv)
 	g_logger = Logger(g_log_identity);
 	// Read config
 	string config_file(argv[1]);
-	g_conf_reader = ConfigReader(config_file);
+	Runner::GetRunner().conf_reader = ConfigReader(config_file);
 
 
-	if (!g_conf_reader.Parse())
+	if (!Runner::GetRunner().conf_reader.Parse())
 		exit(EXIT_FAILURE);
-	parse_config();
+	Runner::ParseConfig();
 	// remember config absolute path
-	g_conf_reader.SetFile(realpath(config_file.c_str(), nullptr));
+	Runner::GetRunner().conf_reader.SetFile(realpath(config_file.c_str(), nullptr));
 	// Check child session id
 	if (setsid() < 0)
 	{
@@ -129,17 +65,17 @@ int main(int argc, char** argv)
 	close(STDERR_FILENO);
 
 	// deamon successfully created, kill other
-	if (!g_pid.KillOthers())
+	if (!Runner::GetRunner().pid.KillOthers())
 		exit(EXIT_FAILURE);
 
 
-	if (!g_pid.Update(getpid(), g_logger))
+	if (!Runner::GetRunner().pid.Update(getpid(), g_logger))
 		exit(EXIT_FAILURE);
 
 	// subscribe for signals
-	signal(SIGHUP, signal_handler);
-	signal(SIGTERM, signal_handler);
+	signal(SIGHUP, Runner::SignalHandler);
+	signal(SIGTERM, Runner::SignalHandler);
 
 	// start working
-	start_proc();
+	Runner::Run();
 }
